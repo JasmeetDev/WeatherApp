c404f9e4958dd94837b6ec4bfbbe7dff170c763e
Vaneet Modgill <vaneet.modgill@sdnatech.com>
2020-01-12 12:32:48 +0530

    Initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Vaneet Modgill on 12/01/20.
+//  Copyright © 2020 Jasmeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
